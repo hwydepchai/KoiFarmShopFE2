e95d715aa788eaedd95ba18199a7c5ff39a23f99
Khuất Trường Huy <cloneacc241@gmail.com>
2024-11-11 05:06:11 +0700

    Update Cart.jsx

--- KoiFarmFE/src/pages/Customer/cart/Cart.jsx
+++ KoiFarmFE/src/pages/Customer/cart/Cart.jsx
@@ -139,11 +139,7 @@ const Cart = () => {
               <tr key={order.id}>
                 <td>{index + 1}</td>
                 <td>{species}</td>
-<<<<<<< HEAD
-                <td>{order.price.toLocaleString()}</td>{" "}
-=======
                 <td>{order.price.toLocaleString()} VND</td>{" "}
->>>>>>> main
                 {/* Use price directly from the order */}
                 <td>{order.status}</td>
                 <td>
