09e6f390fba72cd7eed44e2ae3c548bcb8ad32ce
Khuất Trường Huy <cloneacc241@gmail.com>
2024-11-18 23:06:46 +0700

    disable no use vars

--- KoiFarmFE/src/pages/Guest/card/products.jsx
+++ KoiFarmFE/src/pages/Guest/card/products.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Container, Table, Button, Row, Col, Alert } from "react-bootstrap";
